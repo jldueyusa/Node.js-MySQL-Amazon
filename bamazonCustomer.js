f34745c0104e0110eb13a226fc27ef6bb52c6015
Jennifer Duey <jlduey@gmail.com>
2019-06-11 02:53:44 -0400

    added sql connection parameters and started functions

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -5,4 +5,28 @@ const mysql = require('mysql');
 // Formats SQL data
 require('console.table');
 
+let currentQuantity = "";
+let selectedProduct = "";
+
+// Define the MySQL connection parameters
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 8080,
+    // Your username
+    user: "root",
+    // Your password
+    password: "MmSj.01!",
+    database: "bamazon"
+});
+
+function displayItems() {
+    const query = 'SELECT * FROM products';
+    connection.query(query, (err, res) => {
+        if (err) throw err;
+        console.table(res);
+    });
+    promptPurchase();
+}
+
 
