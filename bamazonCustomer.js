753fff88eaf7860935ea907ee6185c47c992e872
Jennifer Duey <jlduey@gmail.com>
2019-06-11 19:59:20 -0400

    rewrote functions

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,109 +1,78 @@
-//add packages
-const inquirer = require('inquirer');
-const mysql = require('mysql');
+//require packages
+var inquirer  = require('inquirer');
+var mysql = require('mysql');
 
-// Formats SQL data
-require('console.table');
-
-let currentQuantity = "";
-let selectedProduct = "";
-
-// Define the MySQL connection parameters
-
-const connection = mysql.createConnection({
-    host: "localhost",
-    port: 8080,
-    // Your username
-    user: "root",
-    // Your password
-    password: "MmSj.01!",
-    database: "bamazon"
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'MmSj.01!',
+    database: 'bamazon'
 });
 
-//function table
+connection.connect();
 
-function displayItems() {
-    const query = 'SELECT * FROM products';
-    connection.query(query, (err, res) => {
-        if (err) throw err;
-        console.table(res);
+start()
+
+function start (){
+    connection.query('SELECT * FROM products', function (error, res) {
+        if (error) throw error;
+        // console.log(res);
+        res.forEach(row => {
+            console.log(`Id: ${row.item_id} Name: ${row.product_name} Price: ${row.price}\n` )
+        });
+        askQuestions()
+
+        // connection.end()
+    })
+}
+
+function askQuestions() {
+    inquirer.prompt([
+        {
+            message: "Please type in the product id you would like to order.",
+            type: "input",
+            name: "prodId"
+        },
+        {
+            message: "how many of this item would you like to purchase",
+            type: "input",
+            name: "prodQty"
+        }
+    ]).then(function (ans) {
+        var prodId = ans.prodId;
+        var prodQty = ans.prodQty;
+        // var allProd = getAllProd();
+        withdrawProd(prodId, prodQty)
     });
-    promptPurchase();
 }
 
-//function questions-prompts-items and selection options
-
-function promptOptions() {
-    inquirer.prompt({
-            name: "task",
-            type: "list",
-            message: "How can we help you?",
-            choices: [
-                "Shop for Products",
-                "Leave Store"
-            ]
-        })
-        .then(function(answer) {
-            switch (answer.task) {
-                case "Shop for Products":
-                    displayItems();
-                    break;
-                case "Leave Store":
-                    console.log("Thank you for shopping with us");
-                    connection.end();
-                    break;
-            }
-        })
+function withdrawProd(prodId, prodQty) {
+  connection.query('SELECT * FROM products', function (error, res) {
+    if (error) throw error;
+    var prod;
+    // console.log(res);
+    for(var i = 0; i < res.length; i++){
+      if(res[i].item_id == prodId){
+        prod = res[i]
+      }
+    }
+    console.log(prod, "prod was found")
+      if(prod.stock_quantity >= prodQty){
+        orderComplete(prod, prodId, prodQty)
+        connection.end()
+      }else{
+        console.log("sorry the order has been cancled, there was insuffecent stock of this purchase")
+        connection.end()
+      }
+  })
+};
+function orderComplete (prodObj, prodId, prodQty) {
+  var newQuantity = prodObj.stock_quantity - prodQty;
+  var productSales = prodObj.price * prodQty;
+  var queryOne = "UPDATE products SET stock_quantity = ? where ?";
+  var queryTwo = "UPDATE products SET product_sales = ? where ?";
+  connection.query(queryOne,[newQuantity, {item_id: prodId}], function (error, res) {
+  })
+  connection.query(queryTwo, [productSales, { item_id: prodId }], function (error, res) {
+  })
 }
-
-function promptPurchase() {
-    inquirer.prompt([{
-            name: "item",
-            type: "list",
-            message: "What would you like to purchase?",
-            choices: [
-                "Woman's black flip flops",
-                "Men's brown loafers",
-                "Velvet rolledback accent chair",
-                "Black futon sofa",
-                "Mr. Coffee 12 cup coffee maker",
-                "KitchenAid professional mixer",
-                "Apple air pods with charging case",
-                "LG 65inch 4K ultra HD smart LED TV",
-                "Schwinn woman's mountain bike",
-                "Schwinn men's mountain bike"
-            ]
-        }, {
-            name: "quantity",
-            message: "How many would you like to purchase?"
-        }])
-        .then(function(answer) {
-            const query = "SELECT product_name, stock_quantity FROM products WHERE?";
-            connection.query(query, { product_name: answer.item }, function(err, res) {
-                if (err) throw err;
-                if (res[0].stock_quantity > answer.quantity) {
-                    console.log("Item is in stock!");
-                    currentQuantity = (res[0].stock_quantity) - (answer.quantity);
-                    selectedProduct = answer.item;
-                    const query = connection.query("UPDATE products SET ? WHERE?", [{
-                            stock_quantity: currentQuantity
-                        },
-                        {
-                            product_name: selectedProduct
-                        }
-                    ])
-                } else {
-                    console.log("Sorry, there aren't enough items in stock");
-                }
-                promptOptions();
-            });
-        })
-}
-
-//call it
-connection.connect(function(err) {
-    if (err) throw err;
-    displayItems();
-});
-
-
