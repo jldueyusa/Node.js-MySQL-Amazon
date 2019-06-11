110d5a2f41434d5e2d277c9726dd29c8856e44e1
Jennifer Duey <jlduey@gmail.com>
2019-06-11 02:03:37 -0400

    added node packages

--- sql/schema.sql
+++ sql/schema.sql
@@ -2,3 +2,14 @@ DROP DATABASE IF EXISTS bamazon;
 
 CREATE database bamazon;
 USE bamazon;
+
+CREATE TABLE products (
+  position INT AUTO_INCREMENT NOT NULL,
+  item_id INT NOT NULL,
+  product_name VARCHAR(50) NOT NULL,
+  department_name VARCHAR(30) NOT NULL,
+  price DECIMAL(10,2) NOT NULL,
+  stock_quantity INT,
+  UNIQUE (item_id),
+  PRIMARY KEY (position)
+);
